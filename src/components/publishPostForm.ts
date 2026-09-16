5261c901b6fe99db6e1de98bbd6ff5f65b7712e6
Inger Mathilde Eiklund <ingeik06345@stud.noroff.no>
2026-09-16 10:22:31 +0200

    Added the functionality that the user can provide an image url when creating a new post

--- src/components/publishPostForm.ts
+++ src/components/publishPostForm.ts
@@ -11,12 +11,24 @@ interface PublishPost {
   title: string;
   publishPost: string;
   tags: string[];
+  imageUrl: string,
+  imageAlt: string
 }
 interface PublishPostFormElements extends HTMLFormControlsCollection {
   title: HTMLInputElement;
   publishPost: HTMLTextAreaElement;
   tags: HTMLInputElement;
-}
+  imageUrl: HTMLInputElement;
+  imageAlt: HTMLInputElement
+} 
+
+const addOptionalImageBTN = document.getElementById('add-image-button') as HTMLButtonElement;
+addOptionalImageBTN.addEventListener('click', (event) => {
+    event.preventDefault();
+    const optionalImageContainer = document.querySelector('.optional-image-container');
+      optionalImageContainer?.classList.toggle('hidden');
+  })
+
   publishPostForm?.addEventListener("submit", async (event) => {
   event.preventDefault(); 
 
@@ -27,29 +39,36 @@ interface PublishPostFormElements extends HTMLFormControlsCollection {
     "publish-post-button"
   ) as HTMLButtonElement;
 
+ 
   const formData: PublishPost = {
     title: elements.title.value.trim(),
     publishPost: elements.publishPost.value.trim(),
     tags: elements.tags.value
       .split(',')
       .map(tag => tag.trim())
-      .filter((tag) => tag.length > 0)
+      .filter((tag) => tag.length > 0),
+      imageUrl: elements.imageUrl.value.trim(),
+      imageAlt: elements.imageAlt.value.trim(),
   };
+  
   publishPostBTN.disabled = true;
-  if (!formData) {
+  if (!formData.title) {
     toastNotification('Missing input! Please fill out required fields before submitting,', 'warning');
     return;
   }
+  const media = formData.imageUrl ? { url: formData.imageUrl, alt: formData.imageAlt || 'No image added.'} : undefined;
+  publishPostBTN.disabled = true;
+
   try {
 
-    await publishNewPost(formData.title, formData.publishPost, formData.tags);
-    fetchAndRenderPosts();
+    await publishNewPost(formData.title, formData.publishPost, formData.tags, media);
     publishPostForm.reset();
     reRenderPosts();
-
   }catch (error) {
     toastNotification('Something went wrong!', 'error');
     console.log('error');
   }
   publishPostBTN.disabled = false;
 });
+
+
